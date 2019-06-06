7fa9dd68c7f622508c005a81c0df68f5c236ae36
ADMIN <ngominhthien_t62@hus.edu.vn>
2019-06-06 10:07:15 +0700

    BTVN

---
+++ appcrud/config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Tạo ra 4 hằng số để kết nối CSDL
+ */
+//Cách khai báo hằng số
+define("SERVERNAME","localhost");
+define("USERNAME","root");
+define("PASSWORD","");
+define("DBNAME","appcrud");
+
+$connection=mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+
+/**
+ *  Kiểm tra kết nối đến csdl có thành công hay không
+ *  Nếu không thành công sẽ ngắt chương trình
+ */
+
+if(!$connection){
+    die("Kết nối lỗi:" . mysqli_connect_error());
+}
